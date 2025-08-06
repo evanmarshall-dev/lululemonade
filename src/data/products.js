faaacd7e1e4b2dd589334c6f92ac8df1d472e2f4
evanmarshall-dev <me@evanmarshall.dev>
2025-08-05 22:59:36 -0300

    bugfix: Reverted product pricing back to 2 dollars. Changed it while trying to troubleshoot why customers were being charged 2.76 when the payment put through was supposed to be 2.

--- src/data/products.js
+++ src/data/products.js
@@ -6,7 +6,7 @@ export const products = [
   {
     id: 1,
     name: 'Regular Lemonade"',
-    price: 2.76,
+    price: 2,
     description:
       "The classic lemonade made with ice to keep you cool on a hot summer day!",
     imageUrl: regularLemonade,
@@ -14,7 +14,7 @@ export const products = [
   {
     id: 2,
     name: "Raspberry Lemonade",
-    price: 2.76,
+    price: 2,
     description:
       "The classic lemonade with a twist. If you love the tangy flavour of raspberry you will want this lemonade with a burst of raspberry!",
     imageUrl: raspberryLemonade,
@@ -22,7 +22,7 @@ export const products = [
   {
     id: 3,
     name: "Watermelon Lemonade",
-    price: 2.76,
+    price: 2,
     description:
       "Not just an infusion of flavour in this one. It was made by mixing the classic lemonade with a watermelon punch. If you enjoy a scientific masterpiece of flavour you will want to gulp down this one!",
     imageUrl: raspberryLemonade,
