7dabc52776c41012ae285ea9b5cb0c544862d0b8
evanmarshall-dev <me@evanmarshall.dev>
2025-08-06 19:31:42 -0300

    feat: Add product data, new image, and minor style changes on cart page
    This commit adds a new image for watermelon lemonade, updates products object to include new image, and removes button decor on proceed to checkout button.

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,6 @@
 import raspberryLemonade from "../assets/images/raspberry-lemonade.png";
 import regularLemonade from "../assets/images/regular-lemonade.png";
-// import watermelonLemonade from "../assets/images/watermelon-lemonade.png";
+import watermelonLemonade from "../assets/images/watermelon-lemonade.png";
 
 export const products = [
   {
@@ -25,6 +25,6 @@ export const products = [
     price: 2,
     description:
       "Not just an infusion of flavour in this one. It was made by mixing the classic lemonade with a watermelon punch. If you enjoy a scientific masterpiece of flavour you will want to gulp down this one!",
-    imageUrl: raspberryLemonade,
+    imageUrl: watermelonLemonade,
   },
 ];
